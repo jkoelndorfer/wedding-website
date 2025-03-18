05cf21ef1d4dbe7572f39eac9e3459b689494221
John Koelndorfer <john@johnk.io>
2025-03-17 19:42:27 -0500

    Simplify app configuration

--- rsvp/config/config.go
+++ rsvp/config/config.go
@@ -1,7 +1,9 @@
 package config
 
 import (
+	"crypto/rand"
 	"errors"
+	"fmt"
 	"os"
 )
 
@@ -24,11 +26,16 @@ type RSVPConfig interface {
 	// Returns true if this program is currently executing in a local development context; false otherwise.
 	IsLocalDev() bool
 
-	// Returns the name of the DynamoDB table that invites and responses are stored in.
+	// Returns the name of the DynamoDB table that invites and most recent responses are stored in.
 	InvitesDynamoTable() (string, error)
 
 	// Returns the address that this service should bind to.
 	ListenAddress() string
+
+	// Returns the name of the DynamoDB table that the response log is written to.
+	//
+	// All responses are recorded in this table.
+	ResponseLogDynamoTable() (string, error)
 }
 
 type StandardRSVPConfig struct{}
@@ -42,39 +49,17 @@ func New() RSVPConfig {
 // Its purpose is to weed out drive-by bots banging on the API and
 // incurring charges.
 func (c *StandardRSVPConfig) APIClientToken() (string, error) {
-	key, present := os.LookupEnv("API_CLIENT_TOKEN")
-
-	if !present {
-		return "", errors.New("API client token not set in environment")
-	}
-
-	if len(key) < 64 {
-		return "", errors.New("API client token is too short")
-	}
-
-	return key, nil
+	return secretEnv("API_CLIENT_TOKEN", 64)
 }
 
 // This is the secret key used for privileged access to the API.
 //
 // It permits loading invite data.
 func (c *StandardRSVPConfig) APISecretKey() (string, error) {
-	key, present := os.LookupEnv("API_SECRET_KEY")
-
-	if !present {
-		return "", errors.New("authorization key not set in environment")
-	}
-
-	if len(key) < 64 {
-		return "", errors.New("authorization key is too short")
-	}
-
-	return key, nil
+	return secretEnv("API_SECRET_KEY", 64)
 }
 
 // Returns the configured DynamoDB endpoint to use.
-//
-// If the second argument is false, no override should be used.
 func (c *StandardRSVPConfig) DynamoDBEndpoint() (string, bool) {
 	return os.LookupEnv("DYNAMODB_ENDPOINT")
 }
@@ -92,22 +77,65 @@ func (c *StandardRSVPConfig) IsLocalDev() bool {
 }
 
 func (c *StandardRSVPConfig) InvitesDynamoTable() (string, error) {
-	tableName, valueSet := os.LookupEnv("DYNAMODB_INVITES_TABLE")
+	return envOrError("DYNAMODB_INVITES_TABLE")
+}
 
-	if !valueSet {
-		return "", errors.New("DYNAMODB_INVITES_TABLE not set in environment")
-	}
-
-	return tableName, nil
+func (c *StandardRSVPConfig) ResponseLogDynamoTable() (string, error) {
+	return envOrError("DYNAMODB_RESPONSE_LOG_TABLE")
 }
 
 func (c *StandardRSVPConfig) ListenAddress() string {
-	address, valueSet := os.LookupEnv("RSVP_LISTEN_ADDRESS")
-	if !valueSet {
-		address = "127.0.0.1:9000"
+	return envOrDefault("RSVP_LISTEN_ADDRESS", "127.0.0.1:9000")
+}
+
+func envOrError(variableName string) (string, error) {
+	value, valueIsSet := os.LookupEnv(variableName)
+
+	if !valueIsSet {
+		return "", errors.New(fmt.Sprintf("%s required, but not set in the environment", variableName))
 	}
 
-	return address
+	return value, nil
+}
+
+func envOrDefault(variableName string, defaultValue string) string {
+	value, valueIsSet := os.LookupEnv(variableName)
+
+	if !valueIsSet {
+		return defaultValue
+	}
+
+	return value
+}
+
+func secretEnv(variableName string, minLength int) (string, error) {
+	secret, err := envOrError(variableName)
+
+	if err != nil {
+		// Just in case the error we return is not handled properly,
+		// return a very large, random value. This should prevent
+		// accidentally allowing wide-open authentication.
+		return randString(minLength * 4), err
+	}
+
+	if len(secret) < minLength {
+		return "", errors.New(fmt.Sprintf("%s: environment value too short", variableName))
+	}
+
+	return secret, nil
+}
+
+// Generates a random string of length n.
+//
+// Used defensively when secret values cannot be read.
+func randString(n int) string {
+	const charset = "0123456789abcdef"
+	byteArray := make([]byte, n)
+	rand.Read(byteArray)
+	for i, c := range byteArray {
+		byteArray[i] = charset[c%byte(len(charset))]
+	}
+	return string(byteArray)
 }
 
 // Ensure StandardRSVPConfig satisfies the RSVPConfig interface.
